03a49d016768e1073488c6ce92c86d9c6a1cf165
vermelhachan790 <83366923+vermelhachan790@users.noreply.github.com>
2021-04-28 15:39:32 -0300

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	textcolor(GREEN);
+	textbackground(BLACK);
+	int contador=1;
+	while (contador <=99999){
+		printf("********************************###*****##$***********#\n");
+       contador ++;
+
+		}
+	
+	
+	
+	
+	
+	
+	return 0;
+}
